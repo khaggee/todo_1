70b643653f55384f4eb8695b9b40eb80ec9356ce
khaggee <himanshu99mahor@gmail.com>
2025-03-22 18:39:17 +0530

    2nd

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -22,7 +22,7 @@ function TodoForm() {
             <input
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5 "
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 text-white bg-white/20 py-1.5 dark:text-white"
                 value={todo}
                 onChange={(e) => setTodo(e.target.value)}
             />
